229a4489de616aa70dc7c021bacad0f18d882538
benneh <nebbeh@gmail.com>
2024-03-08 20:53:14 -0800

    fix

--- mu.py
+++ mu.py
@@ -362,7 +362,7 @@ def add_bml_roles(game_title):
     
     name_image_pairs, pr_name_image_pairs, wolf_name_image_pairs = load_flavor_jsons()
 
-    villagers = random.sample(name_image_pairs, 6)
+    villagers = random.sample(name_image_pairs, 7)
     powerroles_bml = random.sample(pr_name_image_pairs, 2)
     wolves = random.sample(wolf_name_image_pairs, 2)
 
