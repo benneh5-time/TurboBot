5c9353b9df93e933f35892036f627eeee32b6f02
benneh <nebbeh@gmail.com>
2024-05-26 11:40:46 -0700

    a

--- mafia_roles.py
+++ mafia_roles.py
@@ -1,146 +1,7 @@
-joat_rb_rd_track = {
-    "role": "40",
-    "role_name": "Wolf Jack of All Trades",
-    "faction": "Wolf",
-    "bpv_status": 0,
-    "ninja": 0,
-    "x_shot_limit": 0,
-    "strongman": 0,
-    "even_night": 0,
-    "odd_night": 0,
-    "godfather": 0,
-    "backup": 0,
-    "night_x": 0,
-    "macho": 0,
-    "recluse": 0,
-    "lost": 0,
-    "vengeful": 0,
-    "flipless": 0,
-    "vote_weight": 0,
-    "hide_vote_weight": 0,
-    "non_consecutive": 0,
-    "self_targetable": 0,
-    "treestump": 0,
-    "neighbor": 0,
-    "mason": 0,
-    "lover": 0,
-    "loyal": 0,
-    "disloyal": 0,
-    "uncooperative": 0,
-    "vendor_items": "",
-    "joat": 0,
-    "inventor": 0,
-    "powers": {
-        "roleblock": 1,
-        "redirect": 1,
-        "track": 1
-    },
-    "ita_count": 0,
-    "ita_base_hit": 0,
-    "ita_immunity": 0,
-    "ita_vulnerability": 0,
-    "ita_shield_status": 0,
-    "ita_booster": 0,
-    "ita_nerfer": 0,
-    "character_name": "",
-    "character_image": "",
-    "alignment": "wolf",
-    "faction_color": "#ff2244"
-}
-joat_rc_watch_poison = {
-    "role": "40",
-    "role_name": "Wolf Jack of All Trades",
-    "faction": "Wolf",
-    "bpv_status": 0,
-    "ninja": 0,
-    "x_shot_limit": 0,
-    "strongman": 0,
-    "even_night": 0,
-    "odd_night": 0,
-    "godfather": 0,
-    "backup": 0,
-    "night_x": 0,
-    "macho": 0,
-    "recluse": 0,
-    "lost": 0,
-    "vengeful": 0,
-    "flipless": 0,
-    "vote_weight": 0,
-    "hide_vote_weight": 0,
-    "non_consecutive": 0,
-    "self_targetable": 0,
-    "treestump": 0,
-    "neighbor": 0,
-    "mason": 0,
-    "lover": 0,
-    "loyal": 0,
-    "disloyal": 0,
-    "uncooperative": 0,
-    "vendor_items": "",
-    "joat": 0,
-    "inventor": 0,
-    "powers": {
-        "rolecop": 1,
-        "watch": 1,
-        "poison": 1
-    },
-    "ita_count": 0,
-    "ita_base_hit": 0,
-    "ita_immunity": 0,
-    "ita_vulnerability": 0,
-    "ita_shield_status": 0,
-    "ita_booster": 0,
-    "ita_nerfer": 0,
-    "character_name": "",
-    "character_image": "",
-    "alignment": "wolf",
-    "faction_color": "#ff2244"
-}
+joat_rb_rd_track = {"role": "40", "role_name": "Wolf Jack of All Trades", "faction": "Wolf", "bpv_status": 0, "ninja": 0, "x_shot_limit": 0, "strongman": 0, "even_night": 0, "odd_night": 0, "godfather": 0, "backup": 0, "night_x": 0, "macho": 0, "recluse": 0, "lost": 0, "vengeful": 0, "flipless": 0, "vote_weight": 0, "hide_vote_weight": 0, "non_consecutive": 0, "self_targetable": 0, "treestump": 0, "neighbor": 0, "mason": 0, "lover": 0, "loyal": 0, "disloyal": 0, "uncooperative": 0, "vendor_items": "", "joat": 0, "inventor": 0, "powers": {"roleblock": 1, "redirect": 1, "track": 1}, "ita_count": 0, "ita_base_hit": 0, "ita_immunity": 0, "ita_vulnerability": 0, "ita_shield_status": 0, "ita_booster": 0, "ita_nerfer": 0, "character_name": "", "character_image": "", "alignment": "wolf", "faction_color": "#ff2244"}
+joat_rc_watch_poison = {"role": "40", "role_name": "Wolf Jack of All Trades", "faction": "Wolf", "bpv_status": 0, "ninja": 0, "x_shot_limit": 0, "strongman": 0, "even_night": 0, "odd_night": 0, "godfather": 0, "backup": 0, "night_x": 0, "macho": 0, "recluse": 0, "lost": 0, "vengeful": 0, "flipless": 0, "vote_weight": 0, "hide_vote_weight": 0, "non_consecutive": 0, "self_targetable": 0, "treestump": 0, "neighbor": 0, "mason": 0, "lover": 0, "loyal": 0, "disloyal": 0, "uncooperative": 0, "vendor_items": "", "joat": 0, "inventor": 0, "powers": {"rolecop": 1, "watch": 1, "poison": 1}, "ita_count": 0, "ita_base_hit": 0, "ita_immunity": 0, "ita_vulnerability": 0, "ita_shield_status": 0, "ita_booster": 0, "ita_nerfer": 0, "character_name": "", "character_image": "", "alignment": "wolf", "faction_color": "#ff2244"}
+bpv = {"role": "2", "role_name": "Wolf", "faction": "Wolf", "bpv_status": 1, "ninja": 0, "x_shot_limit": 0, "strongman": 0, "janitor": 0, "even_night": 0, "odd_night": 0, "godfather": 0, "backup": 0, "night_x": 0, "recluse": 0, "vengeful": 0, "flipless": 0, "non_consecutive": 0, "self_targetable": 0, "powers": {"alignment inspection": 0, "fullcop": 0, "rolecop": 0, "bodyguard": 0, "protection": 0, "frame": 0, "jail": 0, "roleblock": 0, "track": 0, "watch": 0, "kill": 0}, "ita_count": 0, "ita_base_hit": 0, "ita_immunity": 0, "ita_vulnerability": 0, "ita_shield_status": 0, "ita_booster": 0, "ita_nerfer": 0, "character_name": "", "character_image": "", "alignment": "wolf", "faction_color": "#ff2244"}
 prk = {"role":"76","role_name":"Wolf Power Role Killer","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":1,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":0,"macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":0,"vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":0,"powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
-bpv = {
-    "role": "2",
-    "role_name": "Wolf",
-    "faction": "Wolf",
-    "bpv_status": 1,
-    "ninja": 0,
-    "x_shot_limit": 0,
-    "strongman": 0,
-    "janitor": 0,
-    "even_night": 0,
-    "odd_night": 0,
-    "godfather": 0,
-    "backup": 0,
-    "night_x": 0,
-    "recluse": 0,
-    "vengeful": 0,
-    "flipless": 0,
-    "non_consecutive": 0,
-    "self_targetable": 0,
-    "powers": {
-        "alignment inspection": 0,
-        "fullcop": 0,
-        "rolecop": 0,
-        "bodyguard": 0,
-        "protection": 0,
-        "frame": 0,
-        "jail": 0,
-        "roleblock": 0,
-        "track": 0,
-        "watch": 0,
-        "kill": 0
-    },
-    "ita_count": 0,
-    "ita_base_hit": 0,
-    "ita_immunity": 0,
-    "ita_vulnerability": 0,
-    "ita_shield_status": 0,
-    "ita_booster": 0,
-    "ita_nerfer": 0,
-    "character_name": "",
-    "character_image": "",
-    "alignment": "wolf",
-    "faction_color": "#ff2244"
-}
 rb_1x = {"role":"13","role_name":"Wolf Roleblocker","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":1,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":0,"macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":1,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":0,"vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":0,"powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 rb_2x = {"role":"13","role_name":"Wolf Roleblocker","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":2,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":0,"macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":1,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":0,"vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":0,"powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 rolecop = {"role":"9","role_name":"Wolf Role Cop","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
@@ -150,7 +11,7 @@ vig_odd_gf = {"role":"11","role_name":"Wolf Vigilante","faction":"Wolf","bpv_sta
 vig_odd = {"role":"11","role_name":"Wolf Vigilante","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":0,"janitor":0,"even_night":0,"odd_night":1,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 fv = {"role":"68","role_name":"Wolf Fruit Vendor","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":1,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":["boob1","threadpull","n1 doc alexa"],"joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 jk = {"role":"15","role_name":"Wolf Jailkeeper","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":1,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
-treestump = {"role":"59","role_name":"Wolf Treestump","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":1,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
+treestump_flipless = {"role":"59","role_name":"Wolf Treestump","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":1,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 watcher_2x = {"role":"21","role_name":"Wolf Watcher","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":2,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 inv_sui_poison_st = {"role":"70","role_name":"Wolf Inventor","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":0,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":1,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{"suicide bomb":1,"poison":1},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
 poisoner_1x = {"role":"51","role_name":"Wolf Poisoner","faction":"Wolf","bpv_status":0,"ninja":0,"x_shot_limit":1,"strongman":0,"janitor":0,"even_night":0,"odd_night":0,"godfather":0,"backup":0,"night_x":"0","macho":0,"recluse":0,"lost":0,"vengeful":0,"flipless":0,"alignment_flip":0,"vote_weight":0,"hide_vote_weight":0,"non_consecutive":0,"self_targetable":0,"treestump":0,"neighbor":0,"mason":0,"lover":0,"loyal":0,"disloyal":0,"uncooperative":0,"blocked":0,"compulsive":"0","vendor_items":"","joat":0,"inventor":0,"disable_in_endgame":"0","powers":{},"ita_count":0,"ita_base_hit":0,"ita_immunity":0,"ita_vulnerability":0,"ita_shield_status":0,"ita_booster":0,"ita_nerfer":0,"character_name":"","character_image":"","alignment":"wolf","faction_color":"#ff2244"}
