753783df8a0153664bd0688cc94e715e82eb4ccf
benneh5-time <nebbeh@gmail.com>
2025-02-06 17:11:06 -0800

    a

--- turbo_bot.py
+++ turbo_bot.py
@@ -2835,7 +2835,7 @@ async def on_reaction_add(reaction, user):
     global game_host_name, player_limit, players, waiting_list, turbo_ping_message   
     if reaction.message.id == turbo_ping_message:
         print(reaction.emoji, flush=True)
-        if reaction.emoji == '<:laserbensdog:1337171130166939739>':
+        if str(reaction.emoji) == '<:laserbensdog:1337171130166939739>':
             if user.id in banned_users:
                 await reaction.message.channel.send("You have been banned for misusing bigping and are not allowed to in turbos.")
                 return
@@ -2888,7 +2888,7 @@ async def on_reaction_add(reaction, user):
 
     if reaction.message.id == status_id:
         print(reaction.emoji, flush=True)
-        if reaction.emoji == '<:laserbensdog:1337171130166939739>':
+        if str(reaction.emoj) == '<:laserbensdog:1337171130166939739>':
             if user.id in banned_users:
                 await reaction.message.channel.send("You have been banned for misusing bigping and are not allowed to in turbos.")
                 return
