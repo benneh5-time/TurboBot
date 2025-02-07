0e20d5bb607556c0a462653d9ef3e74a4060ab34
benneh5-time <nebbeh@gmail.com>
2025-02-06 17:04:15 -0800

    a

--- turbo_bot.py
+++ turbo_bot.py
@@ -2834,7 +2834,7 @@ async def on_reaction_add(reaction, user):
         return
     global game_host_name, player_limit, players, waiting_list, turbo_ping_message   
     if reaction.message.id == turbo_ping_message:
-        if reaction.emoji == '<:laserbensdog:1337171130166939739>':
+        if reaction.emoji == ':laserbensdog:':
             if user.id in banned_users:
                 await reaction.message.channel.send("You have been banned for misusing bigping and are not allowed to in turbos.")
                 return
